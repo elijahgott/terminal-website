130da72073674d16749373a6315f6cdf21481e20
elijahgott <gottelijah@gmail.com>
2026-01-03 16:19:25 -0600

    user input is now added to page after submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('ello')
+const form = document.getElementById('inputForm')
+const input = document.getElementById('terminalInput')
+
+// focus on text input regardles of where is clicked
+document.getElementById('page').addEventListener('click', () => {
+  input.focus()
+})
+
+const log = document.getElementById('log')
+
+// create green user text on log
+const createUserElement = () => {
+  const user = document.createElement('p')
+  user.classList.add('user')
+  user.textContent = 'user:'
+
+  return user
+}
+
+// create element for user input text in log
+const createInputElement = (input) => {
+  const userInput = document.createElement('p')
+  userInput.textContent = input.value
+
+  return userInput
+}
+
+// add text to log
+const submitText = (event) => {
+  event.preventDefault()
+  const newLog = document.createElement('div')
+  newLog.classList.add('logItem')
+
+  const user = createUserElement()
+  const userInput = createInputElement(input)
+
+  newLog.appendChild(user)
+  newLog.appendChild(userInput)
+
+  input.value = ''
+  log.appendChild(newLog)
+}
+
+form.addEventListener('submit', submitText)
+
+const commands = [
+  'help',
+  'hello',
+]
